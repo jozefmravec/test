aed697b137dc2d49cda3e93c6c7f58a3361b4a3d
Jozef Mravec <jojomravec@gmail.com>
2020-11-10 17:23:11 +0100

    Completed exercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,7 @@ package com.appslab;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +13,16 @@ public class Main {
             System.out.println("List je prázdny");
         }
         else arrayList.forEach(System.out::println);
+        
+        List<Integer> numbers3 = arrayList.stream()
+                .filter(num -> num % 2 != 0)
+                .collect(Collectors.toList());
+
+         List<Integer> numbers4 = arrayList.stream()
+                .filter(num -> num % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(numbers3);
+        System.out.println(numbers4);
     }
 }
